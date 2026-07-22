ff9dc855583b8c008206792b688be1c5974c85ae
LucernaSancta <tommaso.pattaro@hotmail.com>
2026-07-22 20:21:02 +0200

    Add rand_new()

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ fn main() {
     let mut grid: [[u16; 4]; 4] = create_grid();
     grid[0][0] = 2;
     println!("{:?}", grid);
+    println!("{}", rand_new());
 }
 
 fn create_grid() -> [[u16; 4]; 4] {
@@ -12,4 +13,12 @@ fn create_grid() -> [[u16; 4]; 4] {
         [0, 0, 0, 0]
     ];
     return grid;
+}
+
+fn rand_new() -> u16 {
+    let n: u8 = rand::random_range(0..10);
+    return match n {
+        0 => 4,
+        _ => 2
+    }
 }

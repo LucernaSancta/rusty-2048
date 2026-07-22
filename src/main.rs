33f54c7baf24ff413d3469864a1a85a5bd7f5b55
LucernaSancta <tommaso.pattaro@hotmail.com>
2026-07-22 19:57:21 +0200

    Add create_grid()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let mut grid: [[u16; 4]; 4] = create_grid();
+    grid[0][0] = 2;
+    println!("{:?}", grid);
+}
+
+fn create_grid() -> [[u16; 4]; 4] {
+    let grid: [[u16; 4]; 4] = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ];
+    return grid;
 }
